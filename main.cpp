e6d5cae391fc68baf9647af9d11883c79ac48e7b
J0hnFisherman <jahoonowak@gmail.com>
2024-02-01 21:22:29 +0100

    czas gry zbierany

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <ctime>
 int main()
 {
-    std::cout << "HELLO WORLD" << std::endl;
+    std::string input;
+    time_t startTime = std::time(nullptr);
 
-    return 0;
+    while (true)
+    {
+        std::time_t currentTime = std::time(nullptr);
+        std::time_t gameDuration = currentTime - startTime;
+
+        std::tm *timeInfo = std::localtime(&gameDuration);
+        std::cout << "Czas Gry: "
+                  << (timeInfo->tm_hour < 10 ? "0" : "") << timeInfo->tm_hour - 1 << ":"
+                  << (timeInfo->tm_min < 10 ? "0" : "") << timeInfo->tm_min << ":"
+                  << (timeInfo->tm_sec < 10 ? "0" : "") << timeInfo->tm_sec << std::endl;
+
+        std::getline(std::cin, input);
+        if (input == "Q" || input == "q")
+        {
+            return 0;
+        }
+    }
 }
