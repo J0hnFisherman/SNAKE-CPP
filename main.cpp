cbaea65711ee3fcecc6f5816133fe7d87e7b3ca3
J0hnFisherman <jahoonowak@gmail.com>
2024-02-01 21:45:03 +0100

    wydzielenie kodu do fukncji

--- main.cpp
+++ main.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 #include <ctime>
+
+void getGameDuration(std::time_t startTime);
+bool isExitDecision(std::string input);
+
 int main()
 {
     std::string input;
-    time_t startTime = std::time(nullptr);
+    std::time_t startTime = std::time(nullptr);
 
     while (true)
     {
-        std::time_t currentTime = std::time(nullptr);
-        std::time_t gameDuration = currentTime - startTime;
-
-        std::tm *timeInfo = std::localtime(&gameDuration);
-        std::cout << "Czas Gry: "
-                  << (timeInfo->tm_hour < 10 ? "0" : "") << timeInfo->tm_hour - 1 << ":"
-                  << (timeInfo->tm_min < 10 ? "0" : "") << timeInfo->tm_min << ":"
-                  << (timeInfo->tm_sec < 10 ? "0" : "") << timeInfo->tm_sec << std::endl;
+        getGameDuration(startTime);
 
         std::getline(std::cin, input);
-        if (input == "Q" || input == "q")
+        if (isExitDecision(input))
         {
             return 0;
         }
     }
+}
+
+void getGameDuration(std::time_t startTime)
+{
+    std::time_t currentTime = std::time(nullptr);
+    std::time_t gameDuration = currentTime - startTime;
+
+    std::tm *timeInfo = std::localtime(&gameDuration);
+    std::cout << "Czas Gry: "
+              << (timeInfo->tm_hour < 10 ? "0" : "") << timeInfo->tm_hour - 1 << ":"
+              << (timeInfo->tm_min < 10 ? "0" : "") << timeInfo->tm_min << ":"
+              << (timeInfo->tm_sec < 10 ? "0" : "") << timeInfo->tm_sec << std::endl;
+}
+
+bool isExitDecision(std::string input)
+{
+
+    return input == "Q" || input == "q";
 }
